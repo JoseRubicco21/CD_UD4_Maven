50d3332185687849ca8992716097e06446ed2bee
JoseRubicco21 <a23joseRF@iessanclemente.net>
2024-05-06 09:06:34 +0200

    Re-generated documentation to add to repo.

--- target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"UD4_Maven","l":"Anagrama"},{"p":"UD4_Maven","l":"App"},{"p":"UD4_Maven","l":"Estudiante"}];updateSearchResults();
