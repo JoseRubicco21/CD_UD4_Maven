01e7b5b2137fa5119580f4e9d183b023cd7dab8c
JoseRubicco21 <a23joseRF@iessanclemente.net>
2024-05-06 09:41:00 +0200

    Created manifest structure and compiled to target.

--- src/test/java/UD4_Maven/AnagramaTest.java
+++ src/test/java/UD4_Maven/AnagramaTest.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class AnagramaTest {
+	
 	@ParameterizedTest
 	@CsvSource({ "abc123,abc123", "ffff,ffff", "mm,nn", "LL,FFFFF" })
-	void checkAnagram(String str1, String str2) {
+		void checkAnagram(String str1, String str2) {
 		Anagrama ana = new Anagrama(str1, str2);
 		assertTrue(ana.checkAnagram(ana.getStr1(), ana.getStr2()));
 	}
